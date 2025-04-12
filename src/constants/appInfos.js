42430fd23469da5e0e99ac3c0524d1908074ef98
Lê Hồng Ngân 20036651 <lehongngan.998@gmail.com>
2025-04-12 22:51:09 +0700

    add base_url deploy

--- src/constants/appInfos.js
+++ src/constants/appInfos.js
@@ -6,7 +6,8 @@ export const appInfo = {
     height: Dimensions.get("window").height,
   },
 
-  BASE_URL: "http://localhost:3000",
+  // BASE_URL: "http://localhost:3000",
+  BASE_URL: "https://childschedulemanagement-server.onrender.com",  // URL đã deploy
   // BASE_URL: "http://192.168.1.27:3000",
   //   BASE_URL: "http://10.0.2.2:3000",
   //   192.168.1.133:8081
